9c455391919eb225b235eaed3c8887949880b53c
Emre Cetinkaya <emrecetinkaya@Emres-MacBook-Pro.local>
2023-05-26 19:02:01 -0700

    Fixed small bug in ExpenseForm.js +enteredAmount

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const enteredData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     };
 
